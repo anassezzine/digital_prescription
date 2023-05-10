07acd12acfe895b9a05aa7aa3cde9a2ef77ed005
kamilia-juba <kamilia.chaker@etu.univ-amu.fr>
2023-05-10 14:53:31 +0200

    affichage  d'ordonnance

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -28,8 +28,8 @@ const AppRoutes: Routes=[
   {path: 'register', component:RegisterComponent},
   {path: 'login', component:LoginComponent},
   {path: 'home', component:HomeComponent, canActivate:[AuthGuard]},
-  {path: 'Ordonnances', component:ListeOrdonnancesComponent},
-  {path: 'ordonnance/:id', component:OrdonnanceComponent},
+  {path: 'Ordonnances', component:ListeOrdonnancesComponent, canActivate:[AuthGuard]},
+  {path: 'ordonnance', component:OrdonnanceComponent, canActivate:[AuthGuard]},
   {path: 'profile', component:ProfileComponent, canActivate:[AuthGuard]},
   {path: 'formOrdonnance', component:FormOrdonnanceComponent, canActivate:[AuthGuard]}
 ]
--- front/src/app/component/ordonnance/ordonnance.component.ts
+++ front/src/app/component/ordonnance/ordonnance.component.ts
@@ -16,14 +16,46 @@ export class OrdonnanceComponent {
     duree: string='';
 
     //déclare une variable qui ne va pas se considérer attribut de la classe et qui va contenir le retour de la fonction getSelectedOrdonnanceId
-    
-
-    
+      
     constructor() {
       
     }    
-
-    
-
-
 };
+
+/*
+import { Component } from '@angular/core';
+export class OrdonnanceParams {
+  constructor(
+    public medecin: string,
+    public date: string,
+    public medicaments: string,
+    public posologie: string,
+    public duree: string
+  ) {}
+}
+
+@Component({
+  selector: 'app-ordonnance',
+  templateUrl: './ordonnance.component.html',
+  styleUrls: ['./ordonnance.component.css']
+})
+export class OrdonnanceComponent {
+  id: number;
+  medecin: string;
+  date: string;
+  medicaments: string;
+  posologie: string;
+  duree: string;
+  
+  static count = 1;
+
+  constructor(params: OrdonnanceParams){
+    this.id = OrdonnanceComponent.count++;
+    this.medecin = params.medecin;
+    this.date = params.date;
+    this.medicaments = params.medicaments;
+    this.posologie = params.posologie;
+    this.duree = params.duree;
+  }
+}
+*/
